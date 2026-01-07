3fcbdb9137c21631146e930133f74907a003193d
haltosan <32181538+haltosan@users.noreply.github.com>
2026-01-06 18:08:02 -0700

    Added distributed day 3

--- 2025/day3.jl
+++ 2025/day3.jl
@@ -1,3 +1,7 @@
+using Distributed
+
+Distributed.addprocs(["worker@pi3"], dir="/home/worker", exename="/home/worker/.juliaup/bin/julia")
+
 function partOne()
 	let tot = 0, first = -1, firstI = -1, second = -1, secondI = -1
 		for line in readlines("inputs/3.txt")
@@ -12,7 +16,7 @@ function partOne()
 	end
 end
 
-function getJoltage(line, remaining)
+@everywhere function getJoltage(line, remaining)
 	if remaining < 0
 		return ""
 	end
@@ -21,10 +25,42 @@ function getJoltage(line, remaining)
 	return string(digit, getJoltage(line[i+1:1:end], remaining-1))
 end
 
-let tot = 0
-	for line in readlines("inputs/3.txt")
-		value = getJoltage(line, 11)
-		tot += parse(Int, value)
+function getJoltageWrapper(chunk, remaining)
+	tot = 0
+	for line in chunk
+		tot += parse(Int, getJoltage(line, remaining))
 	end
-	print("total $(tot)\n")
+	return tot
 end
+
+function partTwo()
+	let tot = 0
+		for line in readlines("inputs/3.txt")
+			value = getJoltage(line, 11)
+			tot += parse(Int, value)
+		end
+		print("total $(tot)\n")
+	end # 175304218462560
+end
+
+function partTwoThreaded()
+	let lines = readlines("inputs/3.txt")
+		chunks = Iterators.partition(lines, cld(length(lines), Threads.nthreads()))
+		tasks = map(chunks) do chunk
+			Threads.@spawn getJoltageWrapper(chunk, 11)
+		end
+		chunk_sums = fetch.(tasks)
+		print("total $(sum(chunk_sums))\n")
+	end
+end
+
+function partTwoDistributed()
+	let lines = readlines("inputs/3.txt")
+		tot = Distributed.@distributed (+) for line in lines
+			parse(Int, getJoltage(line, 11))
+		end
+		print("total $(tot)\n")
+	end
+end
+
+partTwoDistributed()
