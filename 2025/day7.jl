4acff70a9fbb67d48fca536fa4b3b969dd99ee90
haltosan <32181538+haltosan@users.noreply.github.com>
2026-01-12 12:40:53 -0700

    Day 7 part two naive
    Brute force depth first search

--- 2025/day7.jl
+++ 2025/day7.jl
@@ -1,30 +1,54 @@
-let tot = 0, lines = readlines("inputs/7.txt"), prev = split("."^length(lines[begin]), "")
-	for line in lines
-		cur = []
-		onSplit = false
-		for i in firstindex(line):1:lastindex(line)
-			print("$(prev[i]) ")
-			if prev[i] == '|' || prev[i] == 'S'
-				if line[i] == '^'
-					cur[end] = '|'
-					push!(cur, '^')
-					onSplit = true
-					tot += 1
+function partOne()
+	let tot = 0, lines = readlines("inputs/7.txt"), prev = split("."^length(lines[begin]), "")
+		for line in lines
+			cur = []
+			onSplit = false
+			for i in firstindex(line):1:lastindex(line)
+				print("$(prev[i]) ")
+				if prev[i] == '|' || prev[i] == 'S'
+					if line[i] == '^'
+						cur[end] = '|'
+						push!(cur, '^')
+						onSplit = true
+						tot += 1
+					else
+						push!(cur, '|')
+						onSplit = false
+					end
 				else
-					push!(cur, '|')
-					onSplit = false
-				end
-			else
-				if onSplit
-					push!(cur, '|')
-					onSplit = false
-				else
-					push!(cur, line[i])
+					if onSplit
+						push!(cur, '|')
+						onSplit = false
+					else
+						push!(cur, line[i])
+					end
 				end
 			end
+			prev = copy(cur)
+			print("\n")
 		end
-		prev = copy(cur)
-		print("\n")
+		print("total $(tot)\n")
 	end
+end
+
+# depth first search
+let tot = 0, lines = readlines("inputs/7.txt")
+	startI = findnext('S', lines[begin], firstindex(lines[begin]))
+	startLine = firstindex(lines) + 1
+	print("bounds $(lastindex(lines[begin])) $(lastindex(lines))\n")
+	function dfs(i, lineI)
+		if lineI > lastindex(lines)
+			return 1
+		end
+		if i > lastindex(lines[lineI])
+			return 0
+		end
+		if lines[lineI][i] == '.'
+			return dfs(i, lineI+1)
+		else
+			return dfs(i-1, lineI+1) + dfs(i+1, lineI+1)
+		end
+	end
+	tot = dfs(startI, startLine)
 	print("total $(tot)\n")
 end
